559545a574c9c3424e03dd43704e3542d2c877f6
Shubham <shubhamkumarwinner@gmail.com>
2021-07-09 10:35:08 +0530

    Use State in viewModel

--- app/src/main/java/com/shubhamkumarwinner/composestate/todo/TodoActivity.kt
+++ app/src/main/java/com/shubhamkumarwinner/composestate/todo/TodoActivity.kt
@@ -1,17 +1,11 @@
 package com.shubhamkumarwinner.composestate.todo
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.tooling.preview.Preview
 import com.shubhamkumarwinner.composestate.ui.theme.ComposeStateTheme
 
 class TodoActivity : AppCompatActivity() {
@@ -31,7 +25,9 @@ class TodoActivity : AppCompatActivity() {
 
 @Composable
 fun TodoActivityScreen(todoViewModel: TodoViewModel){
-    val items: List<TodoItem> by todoViewModel.todoItems.observeAsState(listOf())
-
-    TodoScreen(items = items, onAddItem = { todoViewModel.addItem(it) }, onRemoveItem = {todoViewModel.removeItem(it)})
+    TodoScreen(
+        items = todoViewModel.todoItems,
+        onAddItem = todoViewModel::addItem,
+        onRemoveItem = todoViewModel::removeItem
+    )
 }
